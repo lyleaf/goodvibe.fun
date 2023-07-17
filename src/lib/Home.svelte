729c86a148894b0ead8d55d4e3ebbec3fa8710bd
Yiling Liu <yilliu@google.com>
2023-07-17 12:36:17 +1000

    change homepage

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -2,15 +2,16 @@
 	<div
 		class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full"
 	>
-		Get curated show <br class="hidden md:block" /> and movie recommendations
-		<br class="hidden md:block" /> with Open AI
+		Create melodic rhymed postcard  <br class="hidden md:block" /> 
+		for your friends and family
+		<br class="hidden md:block" /> to make them happy 
 	</div>
 	<div class="flex flex-wrap items-center mr-auto">
 		<button
 			on:click
 			class="w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 mr-8"
 		>
-			Get recommendations
+			Create my postcard
 		</button>
 		<!-- <a
 			href="https://www.youtube.com/watch?v=Z3LrfdctOpU"
